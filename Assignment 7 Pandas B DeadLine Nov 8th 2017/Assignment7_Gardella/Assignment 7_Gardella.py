b00f7bb5339c5084b62e5fc6de2a58b6c46a7f54
fabiogardella <fabio.gardella1992@gmail.com>
2017-11-06 18:04:05 +0100

    Updating Assignment 7 Gardella

---
+++ Assignment 7 Pandas B DeadLine Nov 8th 2017/Assignment7_Gardella/Assignment 7_Gardella.py
@@ -0,0 +1,27 @@
+import os
+import numpy as np
+import pandas as pd
+os.chdir ("C:\Users\Fabio\Documents\Polimi\Magistrale\Buildings\Assignments\Assignment 7 Gardella")
+
+latitude_location = "40"
+
+
+Beam_irradiance_DF = pd.read_csv("BeamIrradiance.csv",sep=";",index_col=0)
+
+Diffuse_irradiance_DF = pd.read_csv("DiffuseIrradiance.csv",sep=";",index_col=0)
+
+
+
+BeamIrrEast=Beam_irradiance_DF[latitude_location]["E"]
+DiffuseIrrEast=Diffuse_irradiance_DF[latitude_location]["E"]
+
+PXI_east = BeamIrrEast + DiffuseIrrEast
+
+windows_DF = pd.read_csv("windows.csv",sep=";",index_col=0)
+
+windows_DF["PXI"]["east"] = PXI_east
+
+windows_DF.to_csv("windows_completedWithEastPXI.csv",sep=";")
+
+
+
