6f2a2769f8f44b06c3a3e25755ba2d43b1596b6b
cremonaric <cremonaric@gmail.com>
2017-11-06 22:35:40 +0100

    adding assignment7 modified - Cremona

--- Assignment 7 Pandas B DeadLine Nov 8th 2017/Assignment7_Cremona/Assignment7_Cremona.py
+++ Assignment 7 Pandas B DeadLine Nov 8th 2017/Assignment7_Cremona/Assignment7_Cremona.py
@@ -12,9 +12,10 @@ DF_BeamIrradiance = pd.read_csv('BeamIrradiance.csv',sep=';',index_col=0)
 DF_BeamIrradiance_value_east = DF_BeamIrradiance[latitude_location][orientation]
 DF_DiffuseIrradiance = pd.read_csv('DiffuseIrradiance.csv',sep=';',index_col=0)
 DF_DiffuseIrradiance_value_east = DF_DiffuseIrradiance[latitude_location][orientation]
-windows_DF = pd.read_csv('windows_completed_withPXI.csv',sep=';',index_col=0)
+windows_DF = pd.read_csv('windows.csv',sep=';',index_col=0)
+windows_DF['ED'] = np.array([DF_BeamIrradiance_value_east,0,0,0])
+windows_DF['Ed'] = np.array([DF_DiffuseIrradiance_value_east,0,0,0])
 Tx = windows_DF['Tx']['east']
 PXI_value_east = Tx*(DF_BeamIrradiance_value_east+DF_DiffuseIrradiance_value_east)
-windows_DF = pd.read_csv('windows_completed_withPXI.csv',sep=';',index_col=0)
 windows_DF['PXI'] = np.array([PXI_value_east,0,0,0])
 windows_DF.to_csv('windows_completed_withPXI.csv',sep=';')
