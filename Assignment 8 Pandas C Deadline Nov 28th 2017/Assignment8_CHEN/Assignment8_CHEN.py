3c47edfe0643031d074f0b9afa931b8e1ff39234
“IRISCHEN” <crjlyzl1215@gmail.com>
2017-11-27 23:54:10 +0100

    uploading my assignment 8_CHEN

--- Assignment 8 Pandas C Deadline Nov 28th 2017/Assignment8_CHEN/Assignment8_CHEN.py
+++ Assignment 8 Pandas C Deadline Nov 28th 2017/Assignment8_CHEN/Assignment8_CHEN.py
@@ -7,20 +7,24 @@ os.chdir("/Users/chenrujing/Dropbox/Iris/Scientific_Python_Assignments_POLIMI_EE
 BeamIrradiance_DF=pd.read_csv("BeamIrradiance.csv",sep=";",index_col=0) 
 windows_DF=pd.read_csv("windows.csv",sep=";",index_col=0) 
 DiffuseIrradiance_DF=pd.read_csv("DiffuseIrradiance.csv",sep=";",index_col=0) 
-    
+
+
+
+Latitude=42
+name_of_columns=BeamIrradiance_DF.columns.get_values()
+name_of_columns_as_numbers = name_of_columns.astype(np.int32, copy=False)
+
 def PXI_finder(DirectionoftheWall,Latitude):
-    ED=BeamIrradiance_DF.loc[DirectionoftheWall][Latitude]
-    Ed=DiffuseIrradiance_DF.loc[DirectionoftheWall][Latitude]
+    ED=sp.interp(Latitude,name_of_columns_as_numbers,BeamIrradiance_DF.loc[DirectionoftheWall])
+    Ed=sp.interp(Latitude,name_of_columns_as_numbers,DiffuseIrradiance_DF.loc[DirectionoftheWall])
     PXI=ED+Ed
     return PXI   
 
-print windows_DF.index.tolist()    
-DirectionoftheWall=["E","W","S","S"]
-Latitude="45" 
-PXI_Values=PXI_finder(DirectionoftheWall,Latitude).tolist()
+PXI_values=[]
+for index in windows_DF.index.tolist():
+    print index
+    PXI_values = np.append(PXI_values,PXI_finder(windows_DF["Direction"][index],Latitude))
 
-windows_DF["PXI"]=PXI_Values
-windows_DF.to_html("windowsPXI.html")   
-
-
-
+      
+windows_DF["PXI"]=PXI_values
+windows_DF.to_csv("windows_completed_withPXI.csv")
